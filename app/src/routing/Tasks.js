41f4394f9cb97ecc2f2478ab3b53e1603df0a5c3
Sergey Pryanichnikov <s.pryanichnikov@gmail.com>
2019-02-17 14:52:49 +0700

    style bootstrap

--- app/src/routing/Tasks.js
+++ app/src/routing/Tasks.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react'
+import {Button, Form} from "react-bootstrap";
 
-export default class Tasks extends Component{
+export default class Tasks extends Component {
     render() {
         return <div>
             Tasks
+            <br/>
+            <h1> Add task</h1>
+            <Form>
+                <Form.Group controlId="formName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control as="textarea" rows="5"/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+
+
+            </Form>
 
         </div>
     }
